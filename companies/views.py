fc23c054ded67331f67a4b5a09deae37f885cc2f
jinr-07 <rai.john0707@gmail.com>
2024-09-24 19:19:14 +0545

    feat: changed login page as default

--- companies/views.py
+++ companies/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
+
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -8,8 +9,11 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
-            login(request, user)
-            return redirect('company_list')
+            if user.is_superuser:
+                login(request, user)
+                return redirect('company_list')
+            else:
+                return render(request, 'login.html', {'error': 'You are not authorized to access this page.'})
         else:
             return render(request, 'login.html', {'error': 'Invalid username or password'})
 
