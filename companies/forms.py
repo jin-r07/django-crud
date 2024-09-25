80a4aa15a03a04ee1aee7fcd71cf0a9bbd0452e1
jinr-07 <rai.john0707@gmail.com>
2024-09-25 12:20:23 +0545

    feat: minor changes

--- companies/forms.py
+++ companies/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from .models import Company, Student
 
-
+# Form for creating and updating Company instances.
 class CompanyForm(forms.ModelForm):
     class Meta:
-        model = Company
-        fields = ['name']
+        model = Company  # Specify the model this form is associated with.
+        fields = ['name']  # Only the 'name' field will be included in the form.
 
 
+# Form for creating and updating Student instances.
 class StudentForm(forms.ModelForm):
     class Meta:
-        model = Student
-        fields = ['name', 'age']
+        model = Student  # Specify the model this form is associated with.
+        fields = ['name', 'age']  # Include 'name' and 'age' fields in the form.
--- companies/urls.py
+++ companies/urls.py
@@ -1,13 +1,26 @@
 from django.urls import path
 from .views import login_view, company_list, update_company, delete_company, add_student, update_student, delete_student
 
+# Define URL patterns for the application
 urlpatterns = [
+    # Login view for the application
     path('', login_view, name='login'),
+
+    # URL for viewing the list of companies
     path('companies/', company_list, name='company_list'),
+
+    # URL for updating a specific company by its ID
     path('update/<int:company_id>/', update_company, name='update_company'),
+
+    # URL for deleting a specific company by its ID
     path('delete/<int:company_id>/', delete_company, name='delete_company'),
 
+    # URL for adding a new student to a specific company by the company's ID
     path('add_student/<int:company_id>/', add_student, name='add_student'),
+
+    # URL for editing a specific student of a specific company
     path('company/<int:company_id>/student/<int:student_id>/edit/', update_student, name='edit_student'),
+
+    # URL for deleting a specific student of a specific company
     path('company/<int:company_id>/student/<int:student_id>/delete/', delete_student, name='delete_student'),
 ]
