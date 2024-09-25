7a61441ca9843817d92406318ba8fc1db360ad8b
jinr-07 <rai.john0707@gmail.com>
2024-09-25 08:48:12 +0545

    feat: create different db for each company

--- companies/urls.py
+++ companies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import login_view
+from .views import login_view, company_list
 
 urlpatterns = [
     path('', login_view, name='login'),
+    path('companies/', company_list, name='company_list'),
 ]
--- companies/views.py
+++ companies/views.py
@@ -1,6 +1,10 @@
+import os
+import sqlite3
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-
+from .models import Company
+from .forms import CompanyForm
+from django.conf import settings
 
 def login_view(request):
     if request.method == 'POST':
@@ -18,3 +22,25 @@ def login_view(request):
             return render(request, 'login.html', {'error': 'Invalid username or password'})
 
     return render(request, 'login.html')
+
+
+def company_list(request):
+    if request.method == 'POST':
+        form = CompanyForm(request.POST)
+        if form.is_valid():
+            company = form.save()
+
+            company_name = company.name
+            sanitized_company_name = company_name.replace(" ", "_").lower()
+            db_file_path = os.path.join(settings.MEDIA_ROOT, f'{sanitized_company_name}.db')
+
+            conn = sqlite3.connect(db_file_path)
+            conn.close()
+
+            return redirect('company_list')
+    else:
+        form = CompanyForm()
+
+    companies = Company.objects.all()
+
+    return render(request, 'company_list.html', {'companies': companies, 'form': form})
